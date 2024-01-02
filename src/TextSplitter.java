d0c5cdb0e5d9cacf7245def16b998552a38bfe65
Simas <simonas.aruna@gmail.com>
2024-01-02 18:24:47 +0200

    refactoring

---
+++ src/TextSplitter.java
@@ -0,0 +1,12 @@
+public class TextSplitter {
+
+  private String text;
+
+  public TextSplitter(String text) {
+    this.text = text;
+  }
+
+  public String[] getWords() {
+    return text.split(" ");
+  }
+}
