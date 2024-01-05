b29c124ac0a48cac9c533d8e108599bdd6128a66
Simas <simonas.aruna@gmail.com>
2024-01-05 20:16:05 +0200

    endless refactoring

--- src/helpers/output/Printer.java
+++ src/helpers/output/Printer.java
@@ -6,8 +6,16 @@ import java.util.Map;
 import java.util.Set;
 
 public class Printer {
-  private static void printGraph(Set<WordDTO> set) {
+  public static void printGraph(Set<WordDTO> set) {
     for (WordDTO word : set) {
+      System.out.println(word.word + " " + word.links.size());
+      for (Map.Entry<WordDTO, Long> w : word.links.entrySet())
+        System.out.println("     " + w.getKey().word + " " + w.getValue());
+    }
+  }
+  public static void printSpecificWord(Set<WordDTO> set, String specificWord) {
+    for (WordDTO word : set) {
+      if (!word.word.equals(specificWord)) continue;
       System.out.println(word.word + " " + word.links.size());
       for (Map.Entry<WordDTO, Long> w : word.links.entrySet())
         System.out.println("     " + w.getKey().word + " " + w.getValue());
