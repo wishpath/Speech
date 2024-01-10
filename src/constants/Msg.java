cfedc363056338decb1d783aea0cb8d04c0b0189
Simas <simonas.aruna@gmail.com>
2024-01-10 17:49:59 +0200

    removed unfinished sentences

--- src/constants/Msg.java
+++ src/constants/Msg.java
@@ -2,5 +2,5 @@ package constants;
 
 public class Msg {
   public static final String WORD_OBJECT_NOT_FOUND = "OutputSpeechMaker.getWordObjectUsingWordString: No such word Object";
-  public static final String GENERATED_TEXT_INTRODUCTION_START = "\ngenerated text:\n\"%s ";
+  public static final String GENERATED_TEXT_INTRODUCTION_START = "\ngenerated text:\n\"%s";
 }
