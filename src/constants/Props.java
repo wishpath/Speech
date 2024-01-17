b50bce12777a5ffeb763d582e581cdce1bac46e6
Simas <simonas.aruna@gmail.com>
2024-01-17 18:37:49 +0200

    solved plural at the end of sentence like: 'ais.'

--- src/constants/Props.java
+++ src/constants/Props.java
@@ -9,10 +9,12 @@ public class Props {
 
   public static final String[] IGNORED_WORDS_RGX = new String[]{"\\bthe\\b", "\\ba\\b", "\\ban\\b", "\\band\\b"};
   public static final String[] NON_PLURALS = new String[]{"is", "as", "analysis", "chess", "was", "loss", "previous", "various", "linguistics", "this"};
-  public static final String PLURAL_PATTERN = ".*s";
+  public static final String PLURAL_PATTERN_RGX = ".*s\\.?";
+  public static final String ENDS_WITH_DOT_RGX = ".*\\.";
   public static final String IGNORED_WORDS_REPLACEMENT = "";
 
 
   public static final String[] WORDS_WITH_DOTS = new String[]{"e.g."};
   public static final String FAKE_DOT = "\u2024";
+
 }
