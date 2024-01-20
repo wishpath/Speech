efdd29cc2a2864c8ed4ed14bdaa5ee4f3e328aaa
Simas <simonas.aruna@gmail.com>
2024-01-20 23:21:55 +0200

    spearated maps of synonyms

--- src/constants/SimilarWords.java
+++ src/constants/SimilarWords.java
@@ -6,60 +6,63 @@ import java.util.Map;
 
 public class SimilarWords {
   public static final Map<String, List<String>> irregularTenses = new HashMap<>();
-  public static final Map<String, List<String>> synonyms = new HashMap<>(); //this is singular not plural //present simple
+  public static final Map<String, List<String>> SYNONYMS_MULTIPLE_WORDS = new HashMap<>(); //this is singular not plural //present simple
+  public static final Map<String, List<String>> SYNONYMS_ONE_WORD = new HashMap<>(); //this is singular not plural //present simple
   static {
-    irregularTenses.put("be", List.of("is", "was"));
-    synonyms.put("ai", List.of("artificial intelligence"));
-    synonyms.put("human", List.of("people"));
-    synonyms.put("intelligent", List.of("smart"));
-    synonyms.put("technology", List.of("tech"));
-    synonyms.put("use", List.of("utilize"));
-    synonyms.put("computer science", List.of("computational science"));
-    synonyms.put("machine intelligence", List.of("intelligence of machines"));
-    synonyms.put("sophisticated", List.of("advanced"));
-    synonyms.put("chess game", List.of("chess"));
-    synonyms.put("funding reduction", List.of("loss of funding"));
-    synonyms.put("educational institutions", List.of("universities"));
-    synonyms.put("future objectives", List.of("long-term goals"));
-    synonyms.put("software", List.of("program"));
-    synonyms.put("animals", List.of("living beings"));
-    synonyms.put("field of study", List.of("discipline"));
-    synonyms.put("web search engines", List.of("online search platforms"));
-    synonyms.put("recommendation systems", List.of("suggestion systems"));
-    synonyms.put("understanding", List.of("comprehension"));
-    synonyms.put("self-driving cars", List.of("autonomous vehicles"));
-    synonyms.put("generative creative tools", List.of("creative AI tools"));
-    synonyms.put("AI art", List.of("artificial intelligence generated art"));
-    synonyms.put("strategy games", List.of("strategic games"));
-    synonyms.put("Alan Turing", List.of("Turing"));
-    synonyms.put("substantial research", List.of("significant investigation"));
-    synonyms.put("machine intelligence", List.of("machine based intelligence"));
-    synonyms.put("academic discipline", List.of("scholarly field"));
-    synonyms.put("founded", List.of("established"));
-    synonyms.put("cycles of optimism", List.of("phases of hopefulness"));
-    synonyms.put("surpassed", List.of("exceeded"));
-    synonyms.put("transformer architecture", List.of("transformer model"));
-    synonyms.put("ai spring of 2020s", List.of("ai boom in the 2020s"));
-    synonyms.put("companies", List.of("corporations"));
-    synonyms.put("laboratories", List.of("research facilities"));
-    synonyms.put("significant", List.of("notable"));
-    synonyms.put("advance", List.of("progress"));
-    synonyms.put("centered around", List.of("focused on"));
-    synonyms.put("particular goals", List.of("specific objectives"));
-    synonyms.put("use of particular", List.of("utilization of specific"));
-    synonyms.put("goals", List.of("objective"));
-    synonyms.put("reasoning", List.of("logical thinking"));
-    synonyms.put("planning", List.of("strategic thinking"));
-    synonyms.put("learning", List.of("acquiring knowledge"));
-    synonyms.put("natural language processing", List.of("nlp"));
-    synonyms.put("problem solving techniques", List.of("approaches to problem solving"));
-    synonyms.put("search", List.of("exploration"));
-    synonyms.put("mathematical optimization", List.of("math optimization"));
-    synonyms.put("methods based on statistics", List.of("statistical approaches"));
-    synonyms.put("operations research", List.of("operational analysis"));
-    synonyms.put("economics", List.of("economic principles"));
-    synonyms.put("linguistics", List.of("language studies"));
-    synonyms.put("philosophy", List.of("philosophical principles"));
-    synonyms.put("neuroscience", List.of("neural science"));
+    irregularTenses.put("be", List.of("is", "was", "are"));
+    SYNONYMS_ONE_WORD.put("natural language processing", List.of("nlp"));
+    SYNONYMS_ONE_WORD.put("ai", List.of("artificial intelligence"));
+    SYNONYMS_ONE_WORD.put("human", List.of("people"));
+    SYNONYMS_ONE_WORD.put("intelligent", List.of("smart"));
+    SYNONYMS_ONE_WORD.put("technology", List.of("tech"));
+    SYNONYMS_ONE_WORD.put("use", List.of("utilize"));
+    SYNONYMS_ONE_WORD.put("sophisticated", List.of("advanced"));
+    SYNONYMS_ONE_WORD.put("chess game", List.of("chess"));
+    SYNONYMS_ONE_WORD.put("software", List.of("program"));
+    SYNONYMS_ONE_WORD.put("field of study", List.of("discipline"));
+    SYNONYMS_ONE_WORD.put("understanding", List.of("comprehension"));
+    SYNONYMS_ONE_WORD.put("Alan Turing", List.of("Turing"));
+    SYNONYMS_ONE_WORD.put("founded", List.of("established"));
+    SYNONYMS_ONE_WORD.put("surpassed", List.of("exceeded"));
+    SYNONYMS_ONE_WORD.put("companies", List.of("corporations"));
+    SYNONYMS_ONE_WORD.put("significant", List.of("notable"));
+    SYNONYMS_ONE_WORD.put("advance", List.of("progress"));
+    SYNONYMS_ONE_WORD.put("goals", List.of("objective"));
+
+
+
+
+    SYNONYMS_MULTIPLE_WORDS.put("computer science", List.of("computational science"));
+    SYNONYMS_MULTIPLE_WORDS.put("machine intelligence", List.of("intelligence of machines"));
+    SYNONYMS_MULTIPLE_WORDS.put("funding reduction", List.of("loss of funding"));
+    SYNONYMS_MULTIPLE_WORDS.put("educational institutions", List.of("universities"));
+    SYNONYMS_MULTIPLE_WORDS.put("future objectives", List.of("long-term goals"));
+    SYNONYMS_MULTIPLE_WORDS.put("animals", List.of("living beings"));
+    SYNONYMS_MULTIPLE_WORDS.put("web search engines", List.of("online search platforms"));
+    SYNONYMS_MULTIPLE_WORDS.put("recommendation systems", List.of("suggestion systems"));
+    SYNONYMS_MULTIPLE_WORDS.put("self-driving cars", List.of("autonomous vehicles"));
+    SYNONYMS_MULTIPLE_WORDS.put("AI art", List.of("artificial intelligence generated art"));
+    SYNONYMS_MULTIPLE_WORDS.put("strategy games", List.of("strategic games"));
+    SYNONYMS_MULTIPLE_WORDS.put("substantial research", List.of("significant investigation"));
+    SYNONYMS_MULTIPLE_WORDS.put("machine intelligence", List.of("machine based intelligence"));
+    SYNONYMS_MULTIPLE_WORDS.put("academic discipline", List.of("scholarly field"));
+    SYNONYMS_MULTIPLE_WORDS.put("cycles of optimism", List.of("phases of hopefulness"));
+    SYNONYMS_MULTIPLE_WORDS.put("transformer architecture", List.of("transformer model"));
+    SYNONYMS_MULTIPLE_WORDS.put("ai spring of 2020s", List.of("ai boom in the 2020s"));
+    SYNONYMS_MULTIPLE_WORDS.put("laboratories", List.of("research facilities"));
+    SYNONYMS_MULTIPLE_WORDS.put("centered around", List.of("focused on"));
+    SYNONYMS_MULTIPLE_WORDS.put("particular goals", List.of("specific objectives"));
+    SYNONYMS_MULTIPLE_WORDS.put("use of particular", List.of("utilization of specific"));
+    SYNONYMS_MULTIPLE_WORDS.put("reasoning", List.of("logical thinking"));
+    SYNONYMS_MULTIPLE_WORDS.put("planning", List.of("strategic thinking"));
+    SYNONYMS_MULTIPLE_WORDS.put("learning", List.of("acquiring knowledge"));
+    SYNONYMS_MULTIPLE_WORDS.put("problem solving techniques", List.of("approaches to problem solving"));
+    SYNONYMS_MULTIPLE_WORDS.put("mathematical optimization", List.of("math optimization"));
+    SYNONYMS_MULTIPLE_WORDS.put("methods based on statistics", List.of("statistical approaches"));
+    SYNONYMS_MULTIPLE_WORDS.put("operations research", List.of("operational analysis"));
+    SYNONYMS_MULTIPLE_WORDS.put("economics", List.of("economic principles"));
+    SYNONYMS_MULTIPLE_WORDS.put("linguistics", List.of("language studies"));
+    SYNONYMS_MULTIPLE_WORDS.put("philosophy", List.of("philosophical principles"));
+    SYNONYMS_MULTIPLE_WORDS.put("neuroscience", List.of("neural science"));
   }
 }
